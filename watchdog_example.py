de729177242a9be6a23a3ca00624a665a6798b0f
Chip <chiptalkscomputers@gmail.com>
2020-09-27 15:24:22 +0530

    Add files via upload

---
+++ watchdog_example.py
@@ -0,0 +1,32 @@
+#watchdog_example
+
+import watchdog.events
+import watchdog.observers
+import shutil
+
+class Handler(watchdog.events.PatternMatchingEventHandler):
+    def __init__(self):
+        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.txt', '*.png', '*.jpg'], ignore_patterns = None,
+                                                     ignore_directories = False, case_sensitive = True)
+
+    def on_created(self, event):
+        print(f"File was created at {event.src_path}")
+        if event.src_path.endswith('.txt'): 
+            shutil.move(event.src_path, r'C:\Users\win10\Desktop\Text_Documents')
+        elif event.src_path.endswith('.png') or event.src_path.endswith('.jpg'): 
+            shutil.move(event.src_path, r'C:\Users\win10\Desktop\Image_docs')
+    def on_deleted(self, event):
+        print(f"File was deleted at {event.src_path}")
+
+
+event_handler = Handler()
+observer = watchdog.observers.Observer()
+observer.schedule(event_handler, r'C:\Users\win10\Desktop', recursive = False)
+observer.start()
+observer.join()
+
+        
+
+    
+        
+        
